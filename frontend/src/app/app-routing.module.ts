d40555567bda85d3a532693f3a9e2edc9fab6868
mnguemnin <137334019+mnguemnin@users.noreply.github.com>
2023-07-05 13:54:02 +0200

    writing routing file

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RecipeListComponent } from './recipe-list/recipe-list.component';
+import { RecipeComponent } from './recipe/recipe.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path: '', component: RecipeListComponent},
+  {path: 'api/recipes', component: RecipeListComponent},
+  {  path: 'search/:searchTerm', component: RecipeListComponent},
+  {  path: 'api/recipes/:id', component: RecipeComponent}
+ 
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
